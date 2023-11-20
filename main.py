ae9728c3f5258fa5cc1d9928156ac31bfb27293d
admin <coffeechek3@gmail.com>
2023-11-21 01:46:54 +0300

    if function

--- main.py
+++ main.py
@@ -3,3 +3,10 @@ print('Hellow world!')
 name = input("Как Вас зовут? ")
 print("Привет,", name)
 
+a = int(input())
+if a < -5:
+    print('Low')
+elif -5 <= a <= 5:
+    print('Mid')
+else:
+    print('High')
